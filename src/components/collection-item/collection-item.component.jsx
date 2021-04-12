f9ca5cd3d3492f45709503c3a8b70e19e5eea5af
Allfrenk <ale.cata.it@gmail.com>
2021-04-12 22:29:03 +0200

    aggiunto quantity agli items nel cart

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
+import { connect } from "react-redux";
+
+import { addItem } from "../../redux/cart/cart.actions";
+
+import CustonButton from "../custom-button/custom-button.component";
+
 import "./collection-item.style.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div
@@ -16,8 +24,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustonButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustonButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
